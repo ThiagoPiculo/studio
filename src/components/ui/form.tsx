295ca1113925226f306c090638fce67e08bd42dd
Thiago Piculo <xpiculox@gmail.com>
2025-07-04 14:58:02 +0000

    Error: In HTML, <div> cannot be a descendant of <p>. This will cause a h

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -126,13 +127,13 @@ const FormControl = React.forwardRef<
 FormControl.displayName = "FormControl"
 
 const FormDescription = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLParagraphElement>
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => {
   const { formDescriptionId } = useFormField()
 
   return (
-    <p
+    <div
       ref={ref}
       id={formDescriptionId}
       className={cn("text-sm text-muted-foreground", className)}
