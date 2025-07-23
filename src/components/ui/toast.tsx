b4c91bcfc55daff225f5a8facb916cb5fd4462c6
Thiago Piculo <xpiculox@gmail.com>
2025-07-23 22:54:16 +0000

    ok

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -15,13 +15,14 @@ const ToastViewport = React.forwardRef<
   React.ElementRef<typeof ToastPrimitives.Viewport>,
   React.ComponentPropsWithoutRef<typeof ToastPrimitives.Viewport>
 >(({ className, ...props }, ref) => {
+  const isMobile = useIsMobile()
+
   return (
     <ToastPrimitives.Viewport
       ref={ref}
       className={cn(
-        "fixed z-[100] flex max-h-screen w-full flex-col-reverse p-4",
-        "bottom-0 sm:bottom-auto sm:right-0 sm:top-0 sm:flex-col",
-        className
+        "fixed z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:max-w-md",
+        "bottom-0 sm:bottom-auto sm:right-0 sm:top-0 sm:flex-col"
       )}
       {...props}
     />
@@ -82,7 +83,7 @@ const ToastClose = React.forwardRef<
   <ToastPrimitives.Close
     ref={ref}
     className={cn(
-      "absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-100 transition-opacity hover:opacity-100 focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600",
+      "absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-100 transition-opacity hover:text-foreground/70 focus:opacity-100 focus:outline-none focus:ring-2 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600",
       className
     )}
     toast-close=""
