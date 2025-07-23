9dff2f6fdead4e5840e1e98ec8291d03bdaac082
Thiago Piculo <xpiculox@gmail.com>
2025-07-23 22:41:47 +0000

    esta opção deve ser em todos os tipo mobile ou desktop

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -14,7 +14,7 @@ const ToastViewport = React.forwardRef<
   React.ElementRef<typeof ToastPrimitives.Viewport>,
   React.ComponentPropsWithoutRef<typeof ToastPrimitives.Viewport>
 >(({ className, ...props }, ref) => {
-  const isMobile = useIsMobile();
+  const isMobile = useIsMobile()
 
   return (
     <ToastPrimitives.Viewport
@@ -22,7 +22,8 @@ const ToastViewport = React.forwardRef<
       className={cn(
         "fixed z-[100] flex max-h-screen w-full flex-col-reverse p-4",
         // Default to mobile styles, then apply desktop styles if not mobile
-        "bottom-0",
+        isMobile === undefined && 'hidden', // Hide on server to prevent mismatch
+        isMobile === true && "bottom-0",
         isMobile === false && "sm:bottom-auto sm:right-0 sm:top-0 sm:flex-col",
         className
       )}
@@ -85,7 +86,7 @@ const ToastClose = React.forwardRef<
   <ToastPrimitives.Close
     ref={ref}
     className={cn(
-      "absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-0 transition-opacity hover:text-foreground focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600",
+      "absolute right-2 top-2 rounded-md p-1 text-foreground/50 opacity-70 transition-opacity hover:opacity-100 focus:opacity-100 focus:outline-none focus:ring-2 group-hover:opacity-100 group-[.destructive]:text-red-300 group-[.destructive]:hover:text-red-50 group-[.destructive]:focus:ring-red-400 group-[.destructive]:focus:ring-offset-red-600",
       className
     )}
     toast-close=""
