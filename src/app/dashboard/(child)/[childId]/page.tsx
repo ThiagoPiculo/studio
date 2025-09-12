0df334cb80687c0f880b279867afc60bbe6a5108
Thiago Piculo <xpiculox@gmail.com>
2025-09-12 15:41:59 +0000

    ok

--- src/app/dashboard/(child)/[childId]/page.tsx
+++
@@ -1,8 +0,0 @@
-// src/app/dashboard/(child)/[childId]/page.tsx
-'use client';
-
-import { ChildDashboard } from '@/components/dashboard/child/ChildDashboard';
-
-export default function ChildDashboardPage() {
-  return <ChildDashboard />;
-}
